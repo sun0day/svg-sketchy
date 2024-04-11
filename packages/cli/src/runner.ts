fa40ebcfe08578a2026e6d1936ee113d62d4ee06
sun0day <sun_day500@163.com>
2024-04-11 20:57:31 +0800

    chore: add ut

--- packages/cli/src/runner.ts
+++ packages/cli/src/runner.ts
@@ -105,7 +105,7 @@ export class Runner extends EventEmitter {
   }
 
   private resolveAbsPath(path: string) {
-    const f = isAbsolute(path) ? path : join(this.root, path);
+    const f = (isAbsolute(path) ? path : join(this.root, path)).replace(/\\/, '/');
     console.log(f)
     return f
   }
