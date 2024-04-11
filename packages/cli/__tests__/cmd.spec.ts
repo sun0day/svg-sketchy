74a0a54407d3e4a817c8e59a91e2b8d8432fbc99
sun0day <sun_day500@163.com>
2024-04-11 18:03:17 +0800

    chore: add ut

--- packages/cli/__tests__/cmd.spec.ts
+++ packages/cli/__tests__/cmd.spec.ts
@@ -23,7 +23,7 @@ describe('test cmd', () => {
         const svgs = await readSvgs(num);
         expect(svgs.length).toBe(num);
         expect(svgs[0]).not.toBe(global.SVG);
-      }, {timeout: 5000});
+      }, {timeout: 10000});
     }));
   }
 
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    testTimeout: 8000,
+    testTimeout: 10000,
     root: "./packages/cli/",
     setupFiles: resolve(process.cwd(), './vitest.setup.ts'),
     coverage: {
