139202d426547761bf839d05f479ba1d6dcaae02
sun0day <sun_day500@163.com>
2024-04-11 18:00:57 +0800

    chore: add ut

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
+    testTimeout: 8000,
     root: "./packages/cli/",
     setupFiles: resolve(process.cwd(), './vitest.setup.ts'),
     coverage: {
